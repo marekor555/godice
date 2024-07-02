0388a6c933f9ec25cb10fd91cd3b4e89928bdeb6
MAREKOR555 <MAREKOR555@INTERIA.PL>
2024-07-02 14:14:48 +0200

    version alpha

---
+++ main.go
@@ -0,0 +1,120 @@
+package main
+
+import (
+	"fmt"
+	"image/color"
+	"math/rand"
+	"strconv"
+	"time"
+
+	"fyne.io/fyne/v2"
+	"fyne.io/fyne/v2/app"
+	"fyne.io/fyne/v2/canvas"
+	"fyne.io/fyne/v2/container"
+	"fyne.io/fyne/v2/layout"
+	"fyne.io/fyne/v2/widget"
+)
+
+const (
+	SIDES = 6
+)
+
+var (
+	results     = map[int]int{}
+	scoreBoard  *widget.RichText
+	result      *canvas.Text
+	rollAmount  *widget.SelectEntry
+	rollAmounts = []string{
+		"10",
+		"50",
+		"100",
+		"500",
+		"1000",
+		"5000",
+		"10000",
+		"50000",
+		"100000",
+	}
+)
+
+func refreshScoreBoard() {
+	segments := []widget.RichTextSegment{}
+	for i := 1; i <= SIDES; i++ {
+		segments = append(segments, &widget.TextSegment{
+			Text: fmt.Sprintf("%v - %v", i, results[i]),
+		})
+	}
+	scoreBoard.Segments = segments
+	scoreBoard.Refresh()
+}
+
+func rollDice(animation bool) {
+	if animation {
+		for range 10 {
+			result.Text = fmt.Sprint(rand.Intn(SIDES) + 1)
+			result.Refresh()
+			time.Sleep(time.Millisecond * 20)
+		}
+	}
+	finalResult := rand.Intn(SIDES) + 1
+	if animation {
+		result.Text = fmt.Sprint(finalResult)
+		result.Refresh()
+	}
+	results[finalResult]++
+	refreshScoreBoard()
+}
+
+func main() {
+	application := app.New()
+	window := application.NewWindow("godice - dice simulator")
+	window.Resize(fyne.NewSize(250, 300))
+
+	result = canvas.NewText("", color.White)
+	result.TextStyle = fyne.TextStyle{Bold: true}
+	result.TextSize = 50
+	result.Refresh()
+
+	scoreBoard = widget.NewRichText()
+	scoreBoard.Refresh()
+
+	resultAlign := container.NewHBox(layout.NewSpacer(), result, layout.NewSpacer())
+
+	rollOnce := widget.NewButton("roll the dice", func() {
+		go rollDice(true)
+	})
+
+	rollMany := widget.NewButton("roll the amount", func() {
+		amount, err := strconv.Atoi(rollAmount.Text)
+
+		result.Text = "Calculating..."
+		result.Refresh()
+
+		if err != nil {
+			result.Text = "NaN"
+			result.Refresh()
+		}
+		for i := 0; i < amount; i++ {
+			rollDice(false)
+		}
+
+		result.Text = "Done"
+		result.Refresh()
+
+	})
+
+	rollAmount = widget.NewSelectEntry(rollAmounts)
+	rollAmount.SetText(rollAmounts[0])
+
+	rollManyCont := container.NewGridWithColumns(2, rollAmount, rollMany)
+
+	mainCont := container.NewVBox(rollManyCont, rollOnce, layout.NewSpacer(), resultAlign, layout.NewSpacer(), scoreBoard)
+
+	refreshScoreBoard()
+	for i := 1; i <= SIDES; i++ {
+		results[i] = 0
+	}
+
+	window.SetContent(mainCont)
+	window.ShowAndRun()
+}
